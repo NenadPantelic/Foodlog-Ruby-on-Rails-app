12e449e13404295c564218bd8fd920fdeab4e121
NenadPantelic <nenadpantelickg@gmail.com>
2020-10-28 15:07:41 +0100

    Add controller, model and system tests

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with valid attributes" do
+    entry =  Entry.new(meal_type: "Breakfast", calories: 450, proteins: 40, carbohydrates: 30, fats: 20)
+    assert entry.save
+  end
+  # not necessary
+  test "should not save entry without meal_types" do
+    entry = Entry.new(calories:40, proteins: 40, carbohydrates: 30, fats: 20)
+    assert_not entry.save
+  end
+
+  test "should not save entry without calories" do
+    entry = Entry.new(meal_type: "Breakfast", proteins: 40, carbohydrates: 30, fats: 20)
+    assert_not entry.save
+  end
+
+  test "should not save entry without proteins" do
+    entry = Entry.new(meal_type: "Breakfast", calories: 40, carbohydrates: 30, fats: 20)
+    assert_not entry.save
+  end
+
+  test "should not save entry without carbs" do
+    entry = Entry.new(meal_type: "Breakfast", calories: 40, proteins: 30, fats: 20)
+    assert_not entry.save
+  end
+
+  test "should not save entry without fats" do
+    entry = Entry.new(meal_type: "Breakfast", calories: 40, proteins: 30, carbohydrates: 30)
+    assert_not entry.save
+  end
 end
