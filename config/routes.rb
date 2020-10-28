445807ee51e2f80003c3dfa597a7101728ebf08f
NenadPantelic <nenadpantelickg@gmail.com>
2020-10-28 15:40:57 +0100

    Add meal category components

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   get 'archives/index'
   resources :entries
   root to: "entries#index" # entries controller and index action - root url of this app
